2c2b4265df8d09c9586519e5cbcc3bc6b6e22b3a
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-12-01 21:37:46 +0100

    nix: simplify dev environment

--- flake.nix
+++ flake.nix
@@ -1,37 +1,98 @@
-# SPDX-FileCopyrightText: 2024 László Vaskó <vlaci@fastmail.com>
+# SPDX-FileCopyrightText: 2024-2025 László Vaskó <opensource@vlaci.email>
 #
 # SPDX-License-Identifier: EUPL-1.2
 
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
   outputs =
     {
-      self,
       nixpkgs,
-      devenv,
+      git-hooks,
       ...
-    }@inputs:
+    }:
+
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+        }
+      );
+
+      pre-commit-check = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        git-hooks.lib.${system}.run {
+          src = ./.;
+          package = pkgs.prek;
+          hooks = {
+            check-added-large-files.enable = true;
+            end-of-file-fixer.enable = true;
+            check-yaml.enable = true;
+            check-toml.enable = true;
+            nixfmt.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+            cargo-check.enable = true;
+            clippy = {
+              enable = true;
+              packageOverrides.cargo = pkgs.cargo;
+              packageOverrides.clippy = pkgs.clippy;
+            };
+            rustfmt.enable = true;
+            reuse.enable = true;
+          };
+        }
+      );
     in
     {
-      packages.${system} = {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        devenv-test = self.devShells.${system}.default.config.test;
-      };
-
-      devShells.${system}.default = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [ (import ./devenv.nix) ];
-      };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inherit (pre-commit-check.${system}) shellHook;
+            packages =
+              with pkgs;
+              [
+                cargo
+                rustc
+                rustfmt
+                clippy
+                janet
+              ]
+              ++ pre-commit-check.${system}.enabledPackages;
+          };
+        }
+      );
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          inherit (pre-commit-check.${system}.config) package configFile;
+          script = ''
+            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
     };
 }
